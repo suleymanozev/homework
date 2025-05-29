e01c93a1d0d087255f19e54174f8b1d1fdf2106e
Süleyman Özev <suleymanozev@proton.me>
2025-05-29 10:07:12 +0300

    feat: changes

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -12,11 +12,19 @@ function Photos() {
         <>
             <Grid container spacing={2}>
                 <Grid size={12}>
+                    <Typography variant='h3'>
+                        KISA AÇIKLAMA: Gönderdiğiniz verinin büyüklüğü sebebiyle tüm `photo` öğelerini çekip `map`
+                        fonksiyonuyla render'lamak istemedim. Bir nevi sanallaştırma yaparak performanslı olmasını
+                        istedim. Ayrıca `https://jsonplaceholder.typicode.com/photos` adresinden dönen image placeholder
+                        servisi kapanmış onun alternatifini kullanmak durumunda kaldım.
+                    </Typography>
+                </Grid>
+                <Grid size={12}>
                     <Typography variant='h4'>
                         Tüm özellikler aktif
                     </Typography>
                 </Grid>
-                <Grid size={{xs:12, md: 4, lg: 3, xl: 2}}>
+                <Grid size={{xs: 12, md: 4, lg: 3, xl: 2}}>
                     <Carousel items={photos}/>
                 </Grid>
                 <Grid size={10}/>
@@ -25,7 +33,7 @@ function Photos() {
                         Küçük resimler kapalı
                     </Typography>
                 </Grid>
-                <Grid size={{xs:12, md: 4}}>
+                <Grid size={{xs: 12, md: 4}}>
                     <Carousel items={photos} thumbnailsEnabled={false}/>
                 </Grid>
                 <Grid size={8}/>
@@ -34,7 +42,7 @@ function Photos() {
                         Albüm ismi kapalı
                     </Typography>
                 </Grid>
-                <Grid size={{xs:12, md: 6}}>
+                <Grid size={{xs: 12, md: 6}}>
                     <Carousel items={photos} albumEnabled={false}/>
                 </Grid>
                 <Grid size={6}/>
@@ -43,7 +51,7 @@ function Photos() {
                         Göstergeler kapalı
                     </Typography>
                 </Grid>
-                <Grid size={{xs:12, md: 8}}>
+                <Grid size={{xs: 12, md: 8}}>
                     <Carousel items={photos} indicatorsEnabled={false}/>
                 </Grid>
                 <Grid size={4}/>
@@ -52,7 +60,7 @@ function Photos() {
                         Tüm özellikler aktif
                     </Typography>
                 </Grid>
-                <Grid size={{xs:12, md: 10}}>
+                <Grid size={{xs: 12, md: 10}}>
                     <Carousel items={photos}/>
                 </Grid>
                 <Grid size={2}/>
